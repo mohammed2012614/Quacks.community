1f33dca59d823675ed51358e18721ea7bc1c3003
mohammed2012614 <mohammedsalah7300774@gmail.com>
2026-08-06 00:19:15 +0300

    Add community and user action functions in buttons.js

---
+++ js/buttons.js
@@ -0,0 +1,37 @@
+function joinCommunity(){
+
+    window.location.href = "register.html";
+
+}
+
+
+
+function openLogin(){
+
+    window.location.href = "login.html";
+
+}
+
+
+
+function openRegister(){
+
+    window.location.href = "register.html";
+
+}
+
+
+
+function editProfile(){
+
+    alert("Profile editing will be available soon!");
+
+}
+
+
+
+function adminAction(action){
+
+    alert(action + " system is coming soon!");
+
+}
